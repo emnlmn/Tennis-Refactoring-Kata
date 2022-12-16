357d3de472fbb634c0b20fcce85a8b18df7a48c7
Emanuele Menon <emanuele.menon@facile.it>
2022-12-16 16:19:05 +0100

    moved file
    Signed-off-by: Emanuele Menon <emanuele.menon@facile.it>

--- php/src/TennisGame1.php
+++ php/src/TennisGame1.php
@@ -2,81 +2,80 @@
 
 namespace TennisGame;
 
+use TennisGame\Domain\Name;
+use TennisGame\Domain\Player;
+use TennisGame\Domain\Point;
+
 class TennisGame1 implements TennisGame
 {
-    private $m_score1 = 0;
-    private $m_score2 = 0;
-    private $player1Name = '';
-    private $player2Name = '';
+    protected Player $player1;
+    protected Player $player2;
 
-    public function __construct($player1Name, $player2Name)
+    private const SCORES = [
+        0 => 'Love',
+        1 => 'Fifteen',
+        2 => 'Thirty',
+        3 => 'Forty'
+    ];
+
+    public function __construct(string $player1Name, string $player2Name)
     {
-        $this->player1Name = $player1Name;
-        $this->player2Name = $player2Name;
+        $this->player1 = Player::withName($player1Name);
+        $this->player2 = Player::withName($player2Name);
     }
 
     public function wonPoint($playerName)
     {
-        if ('player1' == $playerName) {
-            $this->m_score1++;
-        } else {
-            $this->m_score2++;
+        if ($this->player1->name()->equals(new Name($playerName))) {
+            $this->player1->wonPoint();
         }
+        if ($this->player2->name()->equals(new Name($playerName))) {
+            $this->player2->wonPoint();
+        }
+    }
+
+    private function getScoreWhenEquals(Point $point): string
+    {
+        return match ($point->value()) {
+            0 => "Love-All",
+            1 => "Fifteen-All",
+            2 => "Thirty-All",
+            default => "Deuce",
+        };
+    }
+
+    private function getScoreWhenAdvantage(Player $player1, Player $player2): string
+    {
+        $scoreDelta = $player1->point()->value() - $player2->point()->value();
+
+        return match ($scoreDelta) {
+            1 => "Advantage player1",
+            -1 => "Advantage player2",
+            2, 3, 4 => "Win for player1",
+            default => "Win for player2",
+        };
+    }
+
+    private function getRegularScore(Player $player1, Player $player2): string
+    {
+        return $this->getScoreByPoint($player1->point()) . '-' . $this->getScoreByPoint($player2->point());
+    }
+
+    private function getScoreByPoint(Point $point): string
+    {
+        return self::SCORES[$point->value()];
     }
 
     public function getScore()
     {
-        $score = "";
-        if ($this->m_score1 == $this->m_score2) {
-            switch ($this->m_score1) {
-                case 0:
-                    $score = "Love-All";
-                    break;
-                case 1:
-                    $score = "Fifteen-All";
-                    break;
-                case 2:
-                    $score = "Thirty-All";
-                    break;
-                default:
-                    $score = "Deuce";
-                    break;
-            }
-        } elseif ($this->m_score1 >= 4 || $this->m_score2 >= 4) {
-            $minusResult = $this->m_score1 - $this->m_score2;
-            if ($minusResult == 1) {
-                $score = "Advantage player1";
-            } elseif ($minusResult == -1) {
-                $score = "Advantage player2";
-            } elseif ($minusResult >= 2) {
-                $score = "Win for player1";
-            } else {
-                $score = "Win for player2";
-            }
-        } else {
-            for ($i = 1; $i < 3; $i++) {
-                if ($i == 1) {
-                    $tempScore = $this->m_score1;
-                } else {
-                    $score .= "-";
-                    $tempScore = $this->m_score2;
-                }
-                switch ($tempScore) {
-                    case 0:
-                        $score .= "Love";
-                        break;
-                    case 1:
-                        $score .= "Fifteen";
-                        break;
-                    case 2:
-                        $score .= "Thirty";
-                        break;
-                    case 3:
-                        $score .= "Forty";
-                        break;
-                }
-            }
+        if ($this->player1->point()->equals($this->player2->point())) {
+            return $this->getScoreWhenEquals($this->player1->point());
         }
-        return $score;
+
+        if ($this->player1->point()->value() >= 4 || $this->player2->point()->value() >= 4) {
+            return $this->getScoreWhenAdvantage($this->player1, $this->player2);
+        }
+
+        return $this->getRegularScore($this->player1, $this->player2);
     }
 }
